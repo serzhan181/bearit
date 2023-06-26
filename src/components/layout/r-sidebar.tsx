c77cdec47c55f4ccd3700a7f307eebfccf062170
serzh <serzhan181@outlook.com>
2023-06-26 20:41:12 +0600

    feat: user-nav and auth

--- src/components/layout/r-sidebar.tsx
+++ src/components/layout/r-sidebar.tsx
@@ -1,18 +1,30 @@
 import { Button } from "../ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { UserNav } from "../user-nav";
 
 export const RSidebar = () => {
   return (
     <nav className="fixed top-0 bottom-0 right-0 z-30 border-l w-sidebar border-border">
-      <div className="flex flex-col items-center gap-2 mt-10">
-        <SignUpButton mode="modal">
-          <Button className="w-24">Sign up</Button>
-        </SignUpButton>
-        <SignInButton mode="modal">
-          <Button className="w-24" variant="secondary">
-            Sign in
-          </Button>
-        </SignInButton>
+      <div className="flex flex-col items-center gap-2 mt-10 text-center">
+        <SignedOut>
+          <SignUpButton mode="modal">
+            <Button className="w-24">Sign up</Button>
+          </SignUpButton>
+          <SignInButton mode="modal">
+            <Button className="w-24" variant="secondary">
+              Sign in
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserNav />
+        </SignedIn>
       </div>
     </nav>
   );
---
+++ src/components/user-nav.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Loader, LogOut, PlusCircle, User } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignOutButton, useUser } from "@clerk/nextjs";
+
+export function UserNav() {
+  const { user, isLoaded, isSignedIn } = useUser();
+  if (!isLoaded && !isSignedIn) {
+    return null;
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+          <Avatar className="w-10 h-10">
+            <AvatarImage
+              src={user?.profileImageUrl}
+              alt={user?.username || ""}
+            />
+            <AvatarFallback>{user?.username![0]}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{user?.username}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.emailAddresses[0].emailAddress}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="w-4 h-4 mr-2" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <PlusCircle className="w-4 h-4 mr-2" />
+            <span>Create sub</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <SignOutButton>
+            <div>
+              <LogOut className="w-4 h-4 mr-2" />
+              Sign out
+            </div>
+          </SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
